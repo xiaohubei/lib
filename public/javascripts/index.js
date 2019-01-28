f25b120173bb5ef150a25c7d6c6b942b3df394e0
xingguojie <xingguojie@meituan.com>
2019-01-28 15:38:36 +0800

    init koa2

---
+++ public/javascripts/index.js
@@ -0,0 +1,2 @@
+
+console.log(countNodes(document.body))
---
+++ public/javascripts/lib.js
@@ -0,0 +1,14 @@
+var countNodes = function (node) {
+  //  计算自身
+  var count = 1;
+  //  判断是否存在子节点
+  if (node.hasChildNodes()) {
+    //  获取子节点
+    var cnodes = node.childNodes;
+    //  对子节点进行递归统计
+    for (var i = 0; i < cnodes.length; i++) {
+      count = count + countNodes(cnodes.item(i))
+    }
+  }
+  return count;
+}
